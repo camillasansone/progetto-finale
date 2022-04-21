02eb48105821d5c7aba3bf1c7b4540ea99df8dfe
Your Name <you@example.com>
2022-04-21 11:31:29 +0200

    installed router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import NotFound from './components/not-found/NotFound';
+import Posts from './components/posts/Posts';
+import PostDetail from './components/post-detail/PostDetail';
+import PostsCategory from './components/posts-category/PostsCategory';
+import Contact from './components/contact/Contact';
+import AboutUs from './components/about-us/AboutUs';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Outlet />}>
+          <Route index element={<Posts />} />
+          <Route path="posts/:postId" element={<PostDetail />} />
+          <Route path="posts-categories/:categoryId/posts" element={<PostsCategory />} />
+        </Route>
+
+        <Route path="/contact/:pageId" element={<Contact />} />
+        <Route path="/about-us/:pageId" element={<AboutUs />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
